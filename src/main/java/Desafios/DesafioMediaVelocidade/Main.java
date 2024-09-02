ee7f07b4910aba81e5d8eaaa5ccd0bd2a88d5841
AdmaMotta <admafui@hotmail.com>
2024-09-02 19:21:32 -0300

    Desafio final módulo POO.

---
+++ src/main/java/Desafios/DesafioMediaVelocidade/Main.java
@@ -0,0 +1,37 @@
+package Desafios.DesafioMediaVelocidade;
+
+import java.util.Scanner;
+
+public class Main {
+
+    // Função para calcular a velocidade média de conexão de internet
+    public static double calcularVelocidadeMedia(String [] velocidades) {
+
+        double soma = 0;
+        for (String str : velocidades) {
+            double valor = Double.parseDouble(str);
+            soma += valor;
+        }
+        // TODO: Some todas as velocidades registradas e calcule a média
+        double media = soma / velocidades.length;
+        return media;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Solicitando ao usuário a lista de velocidades de conexão registradas a cada hora
+        String input = scanner.nextLine();
+
+        // Convertendo a entrada em uma lista de strings
+        String[] velocidades = input.split(",");
+
+        // Calculando a velocidade média de conexão
+        double velocidadeMedia = calcularVelocidadeMedia(velocidades);
+
+        // Exibindo a velocidade média de conexão
+        System.out.println((int)velocidadeMedia + " Mbps");
+
+        scanner.close();
+    }
+}
